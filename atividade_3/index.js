43022e737d4ab04e4674c1996d4d60c197f43263
Jonathan Ferreira <j.ferreira28@ymail.com>
2020-06-19 20:24:52 -0300

    atividade 3 ok

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,7 @@
 // ## Importe a biblioteca ##
-
-
-
+let rs = require('readline-sync')
 // ## Faça o código ##
+let ladoQuadrado = rs.questionFloat('Olá, eu posso calcular a área de um quadrado Duvida? Imagine um quadrado e digita o tamanho de um dos lados dele: ')
+
+let resultadoArea =  ladoQuadrado *ladoQuadrado
+console.log("A área do seu quadrado em m² é " + resultadoArea)
