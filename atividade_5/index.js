0d06ad7934fc3bae1c3f69180766a3a8ff0bd139
Jonathan Ferreira <j.ferreira28@ymail.com>
2020-06-22 18:56:24 -0300

    atividade 5 ok

--- atividade_5/index.js
+++ atividade_5/index.js
@@ -1,5 +1,8 @@
 // ## Importe a biblioteca ##
-
-
+let rs = require('readline-sync')
 
 // ## Faça o código ##
+
+let quantidadeCaracteres = rs.question('Digite um texto, eu vou advinhar o numero total de caracteres: ')
+
+console.log("você digitou " + quantidadeCaracteres.length + " caracteres") 
